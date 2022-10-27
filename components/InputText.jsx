bf9c8a55b376347ec98b6cea222921454f7b9714
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-10-27 11:42:37 +0200

    fix: arialabel missing

--- components/InputText.jsx
+++ components/InputText.jsx
@@ -1,12 +1,17 @@
-const InputText = ({textePlaceHolder, bgColor, borderColor, textColor}) => {
-    return(
-        <div>
-            <input type="text" className={`rounded-full border-2 py-1 px-5 ${borderColor} ${bgColor} ${textColor}`} placeHolder={(textePlaceHolder ? textePlaceHolder : 'Default place holder')}/>
-        </div>
-    )
-}
+const InputText = ({ textPlaceHolder, bgColor, borderColor, textColor }) => {
+  return (
+    <div>
+      <input
+        aria-label={textPlaceHolder}
+        type="text"
+        className={`rounded-full border-2 py-1 px-5 ${borderColor} ${bgColor} ${textColor}`}
+        placeHolder={textPlaceHolder || "Default place holder"}
+      />
+    </div>
+  );
+};
 
 // USAGE EXAMPLE:
-// <input textPlaceHolder='Test' borderColor='border-red-400' bgColor='bg-red-300' textColor='text-white'/>
+// <input aria-label="text" textPlaceHolder='Test' borderColor='border-red-400' bgColor='bg-red-300' textColor='text-white'/>
 
 export default InputText;
