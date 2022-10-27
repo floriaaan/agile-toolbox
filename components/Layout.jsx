e7e7daa99c3d58f68367cb76b73881620c4d87df
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-10-27 10:30:44 +0200

    feat: add button

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,6 +1,8 @@
 import { createContext, useContext, useState } from "react";
 import { Navbar } from "./Navbar";
 
+import { CgHashtag } from "react-icons/cg";
+
 const LayoutContext = createContext();
 
 export const LayoutProvider = ({ children }) => {
@@ -22,10 +24,23 @@ export const LayoutProvider = ({ children }) => {
 export const useLayout = () => useContext(LayoutContext);
 
 const LeftSidebar = () => {
-  return <div className="h-full border-r border-[#e5e5e5] w-72">
-    
-  </div>;
+  return (
+    <div className="w-64 h-full border-r border-neutral-200">
+      <div className="p-3 border-b h-2/5 border-neutral-200">
+        <h2 className="font-bold text-neutral-600">Bibliothèque</h2>
+        <div className="flex flex-col mt-4 overflow-y-scroll gap-y-2">
+          <button className="inline-flex items-center p-2 duration-150 border rounded-md hover:bg-neutral-100 active:bg-neutral-200 active:border-neutral-300 border-neutral-200 bg-neutral-50">
+            <CgHashtag className="w-4 h-4 mr-1 text-neutral-400" />
+            Bouton
+          </button>
+        </div>
+      </div>
+      <div className="p-3 h-3/5 border-neutral-200">
+        <h2 className="font-bold text-neutral-600">Composants</h2>
+      </div>
+    </div>
+  );
 };
 const RightSidebar = () => {
-  return <div className="h-full border-l border-[#e5e5e5] w-72"></div>;
+  return <div className="h-full border-l border-neutral-200 w-72"></div>;
 };
