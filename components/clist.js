677007cb5ef0d1e9665c47783077339b70651e26
William-Langlois <langlois.william.pro@gmail.com>
2022-10-27 14:45:47 +0200

    v1

--- components/clist.js
+++ components/clist.js
@@ -1,63 +1,67 @@
 import React from "react";
 import {useState} from "react";
 
-
-export default function Clist({choices,maxchoice}) {
+export default function Clist({choices,maxChoicesNumber,clabel}) {
     const [selectedValue, setSelectedValue] = useState("");
+    const [nbSelected, setNbSelected] = useState(0);
 
+
+    if(clabel == null || clabel == "")
+    {
+        clabel = "select an option"
+    }
+    function valueIsSelected(value)
+    {
+        const isSelected = selectedValue.split(";").some(v => (v === value));
+        console.log(selectedValue);
+        console.log(selectedValue.split(";"));
+        return isSelected;
+    }
     function handleClickOnChoice (choice)
     {
         if(valueIsSelected(choice.value))
         {
             setSelectedValue(selectedValue.replace(choice.value,"").replace(";;",";"));
+            setNbSelected(nbSelected-1)
         }
         else
         {
-            if(maxchoice > 1)
+            if((nbSelected < maxChoicesNumber && selectedValue != "") && selectedValue.length > 0)
             {
-                setSelectedValue( selectedValue+";"+choice.value)
+                console.log("add")
+                setSelectedValue(selectedValue+";"+choice.value)
+                setNbSelected(nbSelected+1)
+
             }
             else
             {
-                setSelectedValue(choice.value);
-            }
-        }
+                if(selectedValue.length > 0)
+                {
 
-        //On retire le pv en trop si besoin
-        if(selectedValue[0] == ";")
-        {
-            setSelectedValue(selectedValue.substring(0,1));
+                }
+                else
+                {
+                    setSelectedValue(choice.value);
+                    setNbSelected(1)
+                }
+            }
         }
         return;
     };
 
-    function valueIsSelected(value)
-    {
-        const isSelected = selectedValue.split(";").some(v => (v == value));
-        return isSelected;
-    }
-    const rendered = [];
-    choices = [
-        {
-            value:"value 1",
-            label:"valeur 1"
-        },
-        {
-            value:"value 2",
-            label:"valeur 2"
-        }
-    ]
-    const getItems = choices.map((choice,i) => {return(
-        <option key={i} className={valueIsSelected(choice.value) ? "selected":""} onClick={() => {handleClickOnChoice(choice)}} selected={valueIsSelected(choice.value)} value={choice.value}>{choice.label}</option>;
-    )});
-    
+    const renderedChoices = choices.map((c) => 
+        valueIsSelected(c.value) 
+            ? <option style={{backgroundColor:"orange" }} onClick={() => {handleClickOnChoice(c)}} value={c.value} key={c.value}>{c.label}</option>
+            : <option style={{backgroundColor:"grey"}} onClick={() => {handleClickOnChoice(c)}} value={c.value} key={c.value}>{c.label}</option>
+    )
+
     return (
+        <div style={{width:"75%;"}}>
+            <label for="choices" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">{clabel}</label>
+            <select name="choices"  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" multiple>
+                { renderedChoices }
+            </select>
+        </div>
         
-        <select name="choices">
-            {{getItems}}
-        </select>
     );
-
-    
-
-}    
+}
