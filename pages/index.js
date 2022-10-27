65d500361356a6f6c63cd5894d9a58345d3be6a6
ophzl <opheliezeitel@gmail.com>
2022-10-27 10:46:20 +0200

    (feat) index: remove button from index

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,6 @@ import Button from "../components/Button";
 
 export default function Home() {
     return (
-        <Button content='Test' borderColor='border-red-400' bgColor='bg-red-300' textColor='text-white'/>
+        <></>
     )
 }
