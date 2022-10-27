d60c33c9bee620a95a6b31d904166710177c9d6f
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-10-27 10:03:41 +0200

    feat: cesi graphical charter

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ["fkscreamer", "Impact,Arial,Helvetica,sans-serif"],
+        body: ["faktum", "Arial, Helvetica, sans-serif"],
+      },
+
+      colors: {
+        primary: "#fbe216",
+        pink: "#f0869d",
+        purple: "#afa5d1",
+      },
+    },
   },
   plugins: [],
 }
