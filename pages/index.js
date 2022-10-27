97866d52a0f568723d4fa2900983a2a3d39faddc
ophzl <opheliezeitel@gmail.com>
2022-10-27 15:20:14 +0200

    (feat) drag&drop

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,31 @@ import InputText from "../components/InputText";
 import Cursor from "../components/Cursor";
 import Exporter from "@components/Exporter";
 import jsPDF from "jspdf";
+import { useRef } from "react";
 
 export default function Home() {
   const { components, setComponents } = useLayout();
+
+  const dragItem = useRef();
+  const dragOverItem = useRef();
+
+  const dragStart = (e, position) => {
+    dragItem.current = position;
+  }
+
+  const dragEnter = (e, position) => {
+    dragOverItem.current = position;
+  };
+
+  const drop = () => {
+    const copyListItems = [...components];
+    const dragItemContent = copyListItems[dragItem.current];
+    copyListItems.splice(dragItem.current, 1);
+    copyListItems.splice(dragOverItem.current, 0, dragItemContent);
+    dragItem.current = null;
+    dragOverItem.current = null;
+    setComponents(copyListItems);
+  };
 
   const printDocument = () => {
     const report = new jsPDF('p', 'pt', 'a4');
@@ -29,14 +51,13 @@ export default function Home() {
                 left: component.position?.x || 0 ,
               }}
               onDragStart={(e) => {
-                const { x, y } = e.target.getBoundingClientRect();
-                console.log(e);
-                // setComponents((components) => {
-                //   const newComponents = [...components];
-                //   newComponents[index].position = { x, y };
-                //   return newComponents;
-                // });
+                dragStart(e, index)
               }}
+              onDragEnter={(e) => {
+                dragEnter(e, index)
+              }}
+              onDragEnd={drop}
+              draggable
             >
               <Button
                 content={component.props?.text}
@@ -55,6 +76,14 @@ export default function Home() {
                 top: component.position?.y || 0 + index * 40,
                 left: component.position?.x || 0 ,
               }}
+              onDragStart={(e) => {
+                dragStart(e, index)
+              }}
+              onDragEnter={(e) => {
+                dragEnter(e, index)
+              }}
+              onDragEnd={drop}
+              draggable
             >
               <InputText
                 textPlaceHolder={component.props?.text}
@@ -91,6 +120,14 @@ export default function Home() {
                 top: component.position?.y || 0 + index * 40,
                 left: component.position?.x || 0 ,
               }}
+              onDragStart={(e) => {
+                dragStart(e, index)
+              }}
+              onDragEnter={(e) => {
+                dragEnter(e, index)
+              }}
+              onDragEnd={drop}
+              draggable
             >
               <Cursor
                 min={component.props?.min}
