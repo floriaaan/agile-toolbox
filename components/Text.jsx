387cb0f6625624a1e49e6bf1b9879fbf53ae1d3e
MatthieuGallice <frooztor@gmail.com>
2022-10-27 15:24:19 +0200

    gestion texte

--- components/Text.jsx
+++ components/Text.jsx
@@ -1,4 +1,4 @@
-const Text = ({ text, height, width, textColor }) => {
+const Texte = ({ text, height, width, textColor }) => {
     return (
       <div>
         <p className={`py-1 px-5 ${height} ${width} ${textColor}`} >
@@ -11,5 +11,5 @@ const Text = ({ text, height, width, textColor }) => {
   // USAGE EXAMPLE:
   // <p line-height='30px' font-size='16px' textColor='text-white'> Example </p>
   
-  export default Text;
+  export default Texte;
   
--- pages/index.js
+++ pages/index.js
@@ -2,35 +2,13 @@ import { useLayout } from "../components/Layout";
 
 import Button from "../components/Button";
 import InputText from "../components/InputText";
-import InputText from "../components/InputText";
+import Texte from "../components/Text";
 import Cursor from "../components/Cursor";
 import Exporter from "@components/Exporter";
 import jsPDF from "jspdf";
-import { useRef } from "react";
 
 export default function Home() {
   const { components, setComponents } = useLayout();
-
-  const dragItem = useRef();
-  const dragOverItem = useRef();
-
-  const dragStart = (e, position) => {
-    dragItem.current = position;
-  }
-
-  const dragEnter = (e, position) => {
-    dragOverItem.current = position;
-  };
-
-  const drop = () => {
-    const copyListItems = [...components];
-    const dragItemContent = copyListItems[dragItem.current];
-    copyListItems.splice(dragItem.current, 1);
-    copyListItems.splice(dragOverItem.current, 0, dragItemContent);
-    dragItem.current = null;
-    dragOverItem.current = null;
-    setComponents(copyListItems);
-  };
 
   const printDocument = () => {
     const report = new jsPDF('p', 'pt', 'a4');
@@ -52,13 +30,14 @@ export default function Home() {
                 left: component.position?.x || 0 ,
               }}
               onDragStart={(e) => {
-                dragStart(e, index)
+                const { x, y } = e.target.getBoundingClientRect();
+                console.log(e);
+                // setComponents((components) => {
+                //   const newComponents = [...components];
+                //   newComponents[index].position = { x, y };
+                //   return newComponents;
+                // });
               }}
-              onDragEnter={(e) => {
-                dragEnter(e, index)
-              }}
-              onDragEnd={drop}
-              draggable
             >
               <Button
                 content={component.props?.text}
@@ -77,14 +56,6 @@ export default function Home() {
                 top: component.position?.y || 0 + index * 40,
                 left: component.position?.x || 0 ,
               }}
-              onDragStart={(e) => {
-                dragStart(e, index)
-              }}
-              onDragEnter={(e) => {
-                dragEnter(e, index)
-              }}
-              onDragEnd={drop}
-              draggable
             >
               <InputText
                 textPlaceHolder={component.props?.text}
@@ -104,7 +75,7 @@ export default function Home() {
                 left: component.position?.x || 0 ,
               }}
             >
-              <Text
+              <Texte
                 text={component.props?.text}
                 height={component.props?.height}
                 width={component.props?.width}
@@ -121,14 +92,6 @@ export default function Home() {
                 top: component.position?.y || 0 + index * 40,
                 left: component.position?.x || 0 ,
               }}
-              onDragStart={(e) => {
-                dragStart(e, index)
-              }}
-              onDragEnter={(e) => {
-                dragEnter(e, index)
-              }}
-              onDragEnd={drop}
-              draggable
             >
               <Cursor
                 min={component.props?.min}
