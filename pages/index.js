4cd591cf04fd49de1f4f4ee4232d7ab33e7b7453
ophzl <opheliezeitel@gmail.com>
2022-10-27 10:44:20 +0200

    (feat) Button: made custom Button component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
+import Button from "../components/Button";
+
 export default function Home() {
     return (
-        <h1>Coucou les loulous</h1>
+        <Button content='Test' borderColor='border-red-400' bgColor='bg-red-300' textColor='text-white'/>
     )
 }
