575cd112ccda4557f7c093ca79e0d945863dd2a2
William-Langlois <langlois.william.pro@gmail.com>
2022-10-27 14:47:24 +0200

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,7 @@
-import Clist from "../components/clist.js";
-
 export default function Home() {
-    const data = [
-        {
-            "value":"value 1",
-            "label":"valeur 1"
-        },
-        {
-            "value":"value 2",
-            "label":"valeur 2"
-        },
-        {
-            "value":"value 3",
-            "label":"valeur 3"
-        },
-        {
-            "value":"value 4",
-            "label":"valeur 4"
-        }
-    ]
     return (
         <div>
             <h1>Coucou les loulous</h1>
-            <Clist choices={data} maxChoicesNumber={2}/>
         </div>
     )
 }
