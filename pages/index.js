1b177d246377d695c325ea5e61b8ab605d1262fa
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-10-27 11:50:46 +0200

    feat: add positionning

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,50 @@
+import { useLayout } from "../components/Layout";
+
 import Button from "../components/Button";
+import InputText from "../components/InputText";
 
 export default function Home() {
-    return (
-        <></>
-    )
+  const { components } = useLayout();
+  return (
+    <div className="relative w-full h-full grow">
+      {components.map((component, index) => {
+        if (component.type === "button") {
+          return (
+            <div
+              className="absolute w-fit h-fit"
+              style={{
+                top: component.position?.y || 0 + index * 40,
+                left: component.position?.x || 0 ,
+              }}
+            >
+              <Button
+                content={component.props?.text}
+                bgColor={component.props?.bgColor}
+                textColor={component.props?.textColor}
+                borderColor={component.props?.borderColor}
+              />
+            </div>
+          );
+        }
+        if (component.type === "textinput") {
+          return (
+            <div
+              className="absolute w-fit h-fit"
+              style={{
+                top: component.position?.y || 0 + index * 40,
+                left: component.position?.x || 0 ,
+              }}
+            >
+              <InputText
+                textPlaceHolder={component.props?.text}
+                bgColor={component.props?.bgColor}
+                textColor={component.props?.textColor}
+                borderColor={component.props?.borderColor}
+              />
+            </div>
+          );
+        }
+      })}
+    </div>
+  );
 }
